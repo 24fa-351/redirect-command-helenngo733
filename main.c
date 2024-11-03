0242c396cdeaa7bc2440d21e3402fcc46ef9dd19
helenngo733 <helenngo@csu.fullerton.edu>
2024-11-03 01:59:29 -0700

    updated w/ parsing & absolute path

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
+#include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include "redir.h"
 
@@ -9,5 +15,64 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    return redir(argv[1], argv[2], argv[3]);
+    char absolute_path[1000];
+    char* words[1000];
+
+    split(argv[2], words, ' ');
+
+    if (!find_absolute_path(words[0], absolute_path)) {
+        printf("Command not found: %s\n", words[0]);
+        return 1;
+    }
+
+    for (int i = 0; words[i] != NULL; i++) {
+        printf("words[%d] = '%s;\n", i, words[i]);
+    }
+    printf("absolute_path = '%s'\n", absolute_path);
+
+    // input and output file descriptors
+    int input_fd;
+    if (strcmp(argv[1], "-") == 0) {
+        input_fd = STDIN_FILENO;
+    } else {
+        input_fd = open(argv[1], O_RDONLY);
+        if (input_fd == -1) {
+            fprintf(stderr, "Failed to open %s\n", argv[1]);
+            return 1;
+        }
+    }
+
+    int output_fd;
+    if (strcmp(argv[3], "-") == 0) {
+        output_fd = STDOUT_FILENO;
+    } else {
+        output_fd = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+        if (output_fd == -1) {
+            fprintf(stderr, "Failed to open %s\n", argv[3]);
+            return 1;
+        }
+    }
+
+    // Fork and execute
+    int child_pid = fork();
+    if (child_pid == 0) {
+        if (input_fd != STDIN_FILENO) {
+            dup2(input_fd, STDIN_FILENO);
+            close(input_fd);
+        }
+        if (output_fd != STDOUT_FILENO) {
+            dup2(output_fd, STDOUT_FILENO);
+            close(output_fd);
+        }
+        execve(absolute_path, words, NULL);
+        fprintf(stderr, "Failed to execute %s\n", absolute_path);
+        exit(1);
+    }
+
+    close(input_fd);
+    close(output_fd);
+
+    wait(NULL);
+
+    return 0;
 }
